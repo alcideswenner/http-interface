b558fb324f06a2695ed7748e0fce8b194ef26bfd
Alcides Wenner <alcides.wenner@itspossiblefor.me>
2023-07-13 12:26:10 -0300

    ♻️ refactor: remove space

--- src/main/java/com/alcideswenner/httpinterface/service/TodoClient.java
+++ src/main/java/com/alcideswenner/httpinterface/service/TodoClient.java
@@ -1,7 +1,6 @@
 package com.alcideswenner.httpinterface.service;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import com.alcideswenner.httpinterface.dto.Todo;
